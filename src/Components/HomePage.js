a90fe5b61dafb59df71342ed6e38db0ab689b40c
Dipesh Makwana <dipesh_makwana@hotmail.co.uk>
2021-01-21 13:23:25 +0000

    Navbar Created

---
+++ src/Components/HomePage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Header from './Header.js';
+
+const HomePage = () => (
+    <>
+        <Header activeLink="#home"></Header>
+        
+    </>
+);
+
+export default HomePage;
